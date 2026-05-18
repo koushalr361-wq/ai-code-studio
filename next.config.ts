84c3bf0093ddb644453408c357bf46a01f7bf2cc
koushalr361-wq <koushalr361@gmail.com>
2026-05-18 17:46:30 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
+  transpilePackages: ['@clerk/nextjs'],
 };
 
 export default nextConfig;
